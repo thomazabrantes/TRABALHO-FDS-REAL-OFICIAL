a6e1473283ed689576ec2bf59425b849354fac69
Thomaz <141247521+thomazabrantes@users.noreply.github.com>
2024-11-24 04:25:17 +0000

    BANCO DE DADOS FUNCIONOU

---
+++ arquitetura_clean_eu_acho/arq_clean_do_zero/trabfdsfinal/target/classes/db/data.sql
@@ -0,0 +1,20 @@
+-- Inserção de dados na tabela de Aplicativos
+INSERT INTO Aplicativo (nome, custo_mensal) VALUES ('Aplicativo de Musica', 17.98);
+INSERT INTO Aplicativo (nome, custo_mensal) VALUES ('Aplicativo de Compras', 5.99);
+INSERT INTO Aplicativo (nome, custo_mensal) VALUES ('Aplicativo de Delivery', 7.50);
+INSERT INTO Aplicativo (nome, custo_mensal) VALUES ('Aplicativo de Viagens', 10.99);
+INSERT INTO Aplicativo (nome, custo_mensal) VALUES ('Aplicativo de Relacionamento', 29.95);
+INSERT INTO Aplicativo (nome, custo_mensal) VALUES ('Aplicativo de Filmes', 15.99);
+
+-- Script para inserir um cliente na tabela Cliente
+INSERT INTO Cliente (nome, email) VALUES ('Joao Silva', 'joao.silva@gmail.com');
+INSERT INTO Cliente (nome, email) VALUES ('Amelia Pinto', 'amelia.pinto@gmail.com');
+INSERT INTO Cliente (nome, email) VALUES ('Janaina Jana', 'janaina.jana@hotmail.com');
+INSERT INTO Cliente (nome, email) VALUES ('Breno Rei', 'reibreno@gmail.com');
+INSERT INTO Cliente (nome, email) VALUES ('Alexandre Costa', 'costaalexandre@gmail.com');
+INSERT INTO Cliente (nome, email) VALUES ('Hebe Amaral', 'hebeamaral@hotmail.com');
+INSERT INTO Cliente (nome, email) VALUES ('Arthur Ricardo', 'arthuuur.ric@gmail.com');
+INSERT INTO Cliente (nome, email) VALUES ('Ema Gonzales', 'gonzalesema@gmail.com');
+INSERT INTO Cliente (nome, email) VALUES ('Haroldo Estilos', 'haroldo.estilos@gmail.com');
+INSERT INTO Cliente (nome, email) VALUES ('Bruna Tramontina', 'tramontina.bruna@hotmail.com');
+INSERT INTO Cliente (nome, email) VALUES ('Eliana Pase', 'elianaapase.pase@gmail.com');
--- arquitetura_clean_eu_acho/arq_clean_do_zero/trabfdsfinal/target/classes/insert.sql
+++
@@ -1,8 +0,0 @@
--- Inserção de aplicativos
-INSERT INTO Aplicativo (nome, custo_mensal) VALUES ('Spotgrid', 69.90);
-INSERT INTO Aplicativo (nome, custo_mensal) VALUES ('Triolingo', 34.90);
-INSERT INTO Aplicativo (nome, custo_mensal) VALUES ('Gustify', 19.90);
-
--- Inserção de clientes
-INSERT INTO Cliente (nome, email) VALUES ('João Silva', 'joao.silva@email.com');
-INSERT INTO Cliente (nome, email) VALUES ('Maria Lima', 'maria.lima@email.com');
