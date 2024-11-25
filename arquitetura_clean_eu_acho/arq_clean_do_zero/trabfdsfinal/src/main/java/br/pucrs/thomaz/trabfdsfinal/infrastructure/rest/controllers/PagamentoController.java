5cec43d839ae8f91737caf3bc7c3a7b8e58784ee
Eduarda Gotuzzo <141246773+megotuzzo@users.noreply.github.com>
2024-11-25 19:50:10 +0000

    PAGAMENTO OK

--- arquitetura_clean_eu_acho/arq_clean_do_zero/trabfdsfinal/src/main/java/br/pucrs/thomaz/trabfdsfinal/infrastructure/rest/controllers/PagamentoController.java
+++ arquitetura_clean_eu_acho/arq_clean_do_zero/trabfdsfinal/src/main/java/br/pucrs/thomaz/trabfdsfinal/infrastructure/rest/controllers/PagamentoController.java
@@ -1,30 +1,25 @@
 package br.pucrs.thomaz.trabfdsfinal.infrastructure.rest.controllers;
 
-import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import br.pucrs.thomaz.trabfdsfinal.application.dto.PagamentoDTO;
-import br.pucrs.thomaz.trabfdsfinal.application.usecase.Pagamento.ListarPagamentosUseCase;
 import br.pucrs.thomaz.trabfdsfinal.application.usecase.Pagamento.PagamentoUseCase;
-import br.pucrs.thomaz.trabfdsfinal.domain.entities.Pagamento;
 
 @RestController
-@RequestMapping("/servcard/pagamentos")
+@RequestMapping("/registrarpagamento")
 public class PagamentoController {
 
     private final PagamentoUseCase pagamentoUseCase;
-    private final ListarPagamentosUseCase listarPagamentosUseCase;
 
-    public PagamentoController(PagamentoUseCase pagamentoUseCase, ListarPagamentosUseCase listarPagamentosUseCase) {
+    public PagamentoController(PagamentoUseCase pagamentoUseCase) {
         this.pagamentoUseCase = pagamentoUseCase;
-        this.listarPagamentosUseCase = listarPagamentosUseCase;
     }
 
     // Endpoint para processar o pagamento
-    @PostMapping("/registrarpagamento")
+    @PostMapping("")
     public ResponseEntity<String> processarPagamento(@RequestBody PagamentoDTO pagamentoDTO) {
         try {
             String resultado = pagamentoUseCase.processarPagamento(
@@ -38,12 +33,5 @@ public class PagamentoController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao processar pagamento.");
         }
-    }
-
-    // Endpoint para listar todos os pagamentos
-    @GetMapping
-    public ResponseEntity<List<Pagamento>> listarPagamentos() {
-        List<Pagamento> pagamentos = listarPagamentosUseCase.listarPagamentos();
-        return ResponseEntity.ok(pagamentos);
     }
 }
